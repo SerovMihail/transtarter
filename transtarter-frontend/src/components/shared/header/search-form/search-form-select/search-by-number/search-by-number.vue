f7f58b0c3a94d493fbe91465c881e88d3a101473
akim1995 <akim.khalitov.ya@gmail.com>
2019-05-17 09:01:07 +0200

    wc sync with nuxt components

--- transtarter-frontend/src/components/shared/header/search-form/search-form-select/search-by-number/search-by-number.vue
+++ transtarter-frontend/src/components/shared/header/search-form/search-form-select/search-by-number/search-by-number.vue
@@ -1,35 +1,41 @@
 <template>
-    <div v-if="type !== 'mobile'" class="search__input search-input search-by-number">
-        <div class="search__input search-input">
-            <div class="search__icon-lins"></div>
-            <input v-model="searchText" type="text" class="search-input-desktop" />
-            <div class="search__clear" @click="clearSearchInput"></div>
-            <search-results
-                v-if="foundItems.length"
-                v-click-outside="closeSearchResult"
-                :found-items="foundItems"
-                class="desktop-search-result"
-                @selected="clearSearchInput"
-            ></search-results>
-        </div>
+  <div v-if="type !== 'mobile'" class="search__input search-input search-by-number">
+    <div class="search__input search-input">
+      <div class="search__icon-lins"></div>
+      <input v-model="searchText" type="text" class="search-input-desktop" />
+      <div class="search__clear" @click="clearSearchInput"></div>
+      <search-results
+        v-if="foundItems.length"
+        v-click-outside="closeSearchResult"
+        :found-items="foundItems"
+        class="desktop-search-result"
+        @selected="clearSearchInput"
+      ></search-results>
+    </div>
+  </div>
+
+  <div v-else class="form-row" style="position: relative;">
+    <div class="search__input-mobile search-input">
+      <input v-model="searchText" type="text" class="search-input-mobile" />
+      <div class="search__clear" @click="clearSearchInput"></div>
     </div>
 
-    <div v-else class="form-row" style="position: relative;">
-        <div class="search__input-mobile search-input">
-            <input v-model="searchText" type="text" class="search-input-mobile" />
-            <div class="search__clear" @click="clearSearchInput"></div>
-        </div>
+    <!-- <nuxt-link tag="button" :to="'/detail-number/' + searchText" class="search__btn-search"> -->
 
-        <!-- <nuxt-link tag="button" :to="'/detail-number/' + searchText" class="search__btn-search"> -->
-            <div class="search__icon-lins"></div>
-        <!-- </nuxt-link> -->
-        <search-results
-            v-if="foundItems.length"
-            v-click-outside="closeSearchResult"
-            :found-items="foundItems"
-            class="mobile-search-result"
-        ></search-results>
-    </div>
+    <a
+      :href="`https://www.tstarter.ru/store/?noScroll=true&searchInput=${searchText}&searchType=1`"
+      class="search__btn-search"
+    >
+      <div class="search__icon-lins"></div>
+    </a>
+    <!-- </nuxt-link> -->
+    <search-results
+      v-if="foundItems.length"
+      v-click-outside="closeSearchResult"
+      :found-items="foundItems"
+      class="mobile-search-result"
+    ></search-results>
+  </div>
 </template>
 <script src="./search-by-number.ts" lang="ts"></script>
 <style src="./search-by-number.scss" lang="scss"></style>
