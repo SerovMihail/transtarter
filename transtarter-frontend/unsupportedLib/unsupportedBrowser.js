cfca07ad5966d06ba2ec72463c39ba09e8c8ab97
Denis A <denis.alimov@koniglabs.ru>
2019-10-03 12:32:31 +0200

    Created a layout

--- transtarter-frontend/unsupportedLib/unsupportedBrowser.js
+++ transtarter-frontend/unsupportedLib/unsupportedBrowser.js
@@ -2,52 +2,33 @@
     var renderBrowserError = d.createElement('body')
     renderBrowserError.innerHTML =
         '<div class="unsupported-browser">' +
-        '<div class="unsupported-browser__container">' +
-        '<div class="unsupported-browser__content-container">' +
-        '<div class="title">' +
-        '<div class="unsupported-browser__title center" =""> ' +
-        'К сожалению, мы не поддерживаем ваш браузер.' +
-        '</div>' +
-        '</div>' +
-        '<div class="app-modal__message center" =""> ' +
-        'Пожалуйста, установите один из поддерживаемых нами браузеров последней версии.' +
-        '</div>' +
-        '<div class="unsupported-browser__browsers-container" ="">' +
-        '<div class="unsupported-browser__browser-container" =""><a' +
-        'href="https://www.mozilla.org/ru/firefox/new" ="">' +
-        '<div class="unsupported-browser__icon unsupported-browser__icon--firefox" =""></div>' +
-        '</a> <a href="https://www.mozilla.org/ru/firefox/new" class="unsupported-browser__chrome-text"' +
-        '="">Firefox</a></div>' +
-        '<div class="unsupported-browser__browser-container" =""><a href="https://www.google.com/chrome"' +
-        '="">' +
-        '<div class="unsupported-browser__icon unsupported-browser__icon--chrome" =""></div>' +
-        '</a> <a href="https://www.google.com/chrome" class="unsupported-browser__chrome-text"' +
-        '="">Google Chrome</a></div>' +
-        '<div class="unsupported-browser__browser-container" =""><a href="https://www.opera.com/ru"' +
-        '="">' +
-        '<div class="unsupported-browser__icon unsupported-browser__icon--opera" =""></div>' +
-        '</a> <a href="https://www.opera.com/ru" class="unsupported-browser__chrome-text" ="">Opera</a>' +
-        '</div>' +
-        '</div>' +
-        '<div class="unsupported-browser__old-site-version" ="">' +
-        'Или перейдите на старую версию сайта ' +
-        '<a href="' +
-        fallbackUrl +
-        '">' +
-        fallbackUrl +
-        '</a></div>' +
-        '</div>' +
-        '</div>' +
+          '<div class="unsupported-browser__container">' +
+            '<div class="unsupported-browser__content-container">' +
+              '<div class="title">' +
+                '<div class="unsupported-browser__title center" =""> ' +
+                  'Вы используете Windows XP, данная операционная система не поддерживается данным сайтом' +
+                '</div>' +
+              '</div>' +
+
+              '<div class="unsupported-browser__old-site-version" ="">' +
+                'Пожалуйста перейдите на предыдущую версию сайта ' +
+                '<a class="unsupported-browser__link" href="' + fallbackUrl + '">' + fallbackUrl + '</a>' +
+              '</div>' +
+    '<div class="unsupported-browser__instruction">' +
+    '<div class="unsupported-browser__instruction-text">' +
+      'и нажмите "авторизоваться'+
+    '</div>' +
+    '<img class="unsupported-browser__instruction-img" src="./assets/instruction.png" >' +
+    '<div style="clear: both;"></div>'
+    '</div>' +
+            '</div>' +
+          '</div>' +
         '</div>'
     var isValidBrowserVersion = true
     var clientBrowser = bowser.getParser(navigator.userAgent)
     var clientBrowserEngine = clientBrowser.parseEngine().name
     if (clientBrowser.is('Chrome')) {
-<<<<<<< HEAD
         isValidBrowserVersion = clientBrowser.satisfies({ chrome: '>49.0.2623.112' })
-=======
-        isValidBrowserVersion = clientBrowser.satisfies({ chrome: '>49.0.2623.75' })
->>>>>>> f0340e0bcb943496162ab408782971b68c685d33
     }
     if (clientBrowserEngine === 'EdgeHTML') {
         w.location.href = fallbackUrl
