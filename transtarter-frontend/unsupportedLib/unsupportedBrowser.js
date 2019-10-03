b1bfd727ca207229da36ff8c56f6854bfd479b3e
Denis A <denis.alimov@koniglabs.ru>
2019-10-03 13:13:38 +0200

    Changed a layout

--- transtarter-frontend/unsupportedLib/unsupportedBrowser.js
+++ transtarter-frontend/unsupportedLib/unsupportedBrowser.js
@@ -5,20 +5,26 @@
           '<div class="unsupported-browser__container">' +
             '<div class="unsupported-browser__content-container">' +
               '<div class="title">' +
-                '<div class="unsupported-browser__title center" =""> ' +
+                '<h1 class="unsupported-browser__title center" =""> ' +
+                  'К сожалению, мы не поддерживаем Ваш браузер' +
+                '</h1>' +
+              '</div>' +
+              '<div class="subtitle">' +
+                '<div class="unsupported-browser__subtitle center" =""> ' +
                   'Вы используете Windows XP, данная операционная система не поддерживается данным сайтом' +
                 '</div>' +
               '</div>' +
-
               '<div class="unsupported-browser__old-site-version" ="">' +
                 'Пожалуйста перейдите на предыдущую версию сайта ' +
                 '<a class="unsupported-browser__link" href="' + fallbackUrl + '">' + fallbackUrl + '</a>' +
               '</div>' +
     '<div class="unsupported-browser__instruction">' +
     '<div class="unsupported-browser__instruction-text">' +
-      'и нажмите "авторизоваться'+
+      'и нажмите "авторизоваться", следуя инструкции ниже'+
     '</div>' +
-    '<img class="unsupported-browser__instruction-img" src="./assets/instruction.png" >' +
+    '<div class="unsupported-browser__instruction-img">' +
+      '<img src="./assets/instruction.png" >' +
+    '</div>'
     '<div style="clear: both;"></div>'
     '</div>' +
             '</div>' +
